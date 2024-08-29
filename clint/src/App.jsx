9bc6e014f5f5539fc5584f6ade7b243f4036688a
Mayank Singh <mayank23378@gmail.com>
2024-08-29 20:44:14 +0530

    update header

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -7,6 +7,7 @@ import About from './pages/About';
 import Profile from './pages/Profile';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
@@ -17,7 +18,9 @@ export default function App() {
          <Route path='/sign-in' element={<SignIn/>} />
          <Route path='/sign-up' element={<SignUp/>} />
          <Route path='/about' element={<About/>} />
-         <Route path='/profile' element={<Profile/>} />
+         <Route element={<PrivateRoute/>} >    
+            <Route path='/profile' element={<Profile/>} />
+         </Route>
        </Routes>
        <Footer/>
     </BrowserRouter>
