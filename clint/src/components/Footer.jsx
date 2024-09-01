2d0969e8dc06b877d647ee81ffbee4171563d240
Mayank Singh <mayank23378@gmail.com>
2024-09-01 11:32:42 +0530

    ui chnages of text

--- clint/src/components/Footer.jsx
+++ clint/src/components/Footer.jsx
@@ -2,18 +2,24 @@ import { FaLinkedin,FaGithub,FaTwitter } from "react-icons/fa";
 
 export default function Footer() {
   return (
-    <footer className="bg-slate-200 shadow-md w-auto">
-      <div className="flex text-gray-700 justify-center gap-10 text-3xl py-5">
+    <footer className="bg-slate-200 shadow-md w-auto ">
+      <div className="flex text-mixup justify-center gap-10 text-3xl pt-5">
         <a href="https://github.com/Mayank-singh-01" target="_blank">
           <FaGithub />
         </a>
-        <a href="https://www.linkedin.com/in/mayank-singh-a76577266/" target="_blank">
+        <a
+          href="https://www.linkedin.com/in/mayank-singh-a76577266/"
+          target="_blank"
+        >
           <FaLinkedin />
         </a>
         <a href="https://twitter.com/__mayank_singh_" target="_blank">
           <FaTwitter />
         </a>
       </div>
+      <p className="text-center text-gray-800 text-sm pt-2 pb-4">
+        Copyright © 2024. Nothing are reserved.
+      </p>
     </footer>
   );
 }
--- clint/src/components/Header.jsx
+++ clint/src/components/Header.jsx
@@ -11,9 +11,9 @@ export default function Header() {
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          <h1 className="font-bold text-sm sm:text-xl text-mixup flex flex-wrap">
             <span className="text-slate-500">My</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="">Estate</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
