83ef78b132c95b1921f5a938774e7810417937d5
Mayank Singh <mayank23378@gmail.com>
2024-09-02 18:21:43 +0530

    update listing

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -9,6 +9,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
+import UpdateListing from './pages/UpdateListing'
 
 export default function App() {
   return (
@@ -22,6 +23,8 @@ export default function App() {
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
+          <Route path="/update-listing/:listingId" element={<UpdateListing/>}
+          />
         </Route>
       </Routes>
       <Footer />
