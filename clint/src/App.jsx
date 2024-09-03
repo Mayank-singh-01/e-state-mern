cf3dc88388cfc63c66c5cb7f3fbba1c5f06a2545
Mayank Singh <mayank23378@gmail.com>
2024-09-03 10:46:36 +0530

    show listing page publically

--- clint/src/App.jsx
+++ clint/src/App.jsx
@@ -10,6 +10,7 @@ import Footer from './components/Footer';
 import PrivateRoute from './components/PrivateRoute';
 import CreateListing from './pages/CreateListing';
 import UpdateListing from './pages/UpdateListing'
+import Listing  from './pages/Listing'
 
 export default function App() {
   return (
@@ -20,11 +21,11 @@ export default function App() {
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/about" element={<About />} />
+        <Route path="/listing/:listingId" element={<Listing />} />
         <Route element={<PrivateRoute />}>
           <Route path="/profile" element={<Profile />} />
           <Route path="/create-listing" element={<CreateListing />} />
-          <Route path="/update-listing/:listingId" element={<UpdateListing/>}
-          />
+          <Route path="/update-listing/:listingId" element={<UpdateListing />}/>
         </Route>
       </Routes>
       <Footer />
--- clint/src/pages/CreateListing.jsx
+++ clint/src/pages/CreateListing.jsx
@@ -265,7 +265,7 @@ export default function CreateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">($ / month)</span>
+                {formData.type === "rent" && ( <span className="text-xs">($ / month)</span> )}
               </div>
             </div>
             {formData.offer && (
@@ -282,7 +282,7 @@ export default function CreateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted price</p>
-                  <span className="text-xs">($ / month)</span>
+                  {formData.type === "rent" && ( <span className="text-xs">($ / month)</span> )}
                 </div>
               </div>
             )}
@@ -343,7 +343,7 @@ export default function CreateListing() {
         >
           {loading ? "Creating..." : "Create listing"}
         </button>
-        {error && <p className="text-red-700 text-sm" >{error}</p>}
+        {error && <p className="text-red-700 text-sm">{error}</p>}
       </form>
     </main>
   );
