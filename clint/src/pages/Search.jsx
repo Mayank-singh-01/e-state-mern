bdcc0fa2a0d94ac633e695406d7742f7f3586dc3
Mayank Singh <mayank23378@gmail.com>
2024-09-05 22:11:57 +0530

    add show more listings funtionality

--- clint/src/pages/Search.jsx
+++ clint/src/pages/Search.jsx
@@ -17,6 +17,7 @@ export default function Search() {
 
    const [loading, setLoading] = useState(false);
    const [listings, setListings] = useState([]);
+   const [showMore, setShowMore] = useState(false);
 
    useEffect(()=>{
       const urlParams = new URLSearchParams(location.search);
@@ -43,9 +44,15 @@ export default function Search() {
       const fetchListings = async () => {
         try {
           setLoading(true);
+          setShowMore(false);
           const searchQuery = urlParams.toString();
           const res = await fetch(`/api/listing/get?${searchQuery}`);
           const data = await res.json();
+          if(data.length > 8) {
+            setShowMore(true);
+          } else{
+            setShowMore(false);
+          }
           setListings(data);
           setLoading(false);
         } catch (error) {
@@ -92,6 +99,20 @@ export default function Search() {
       const searchQuery = urlParams.toString();
       navigate(`/search?${searchQuery}`);
    }
+
+     const onShowMoreClick = async () => {
+       const numberOfListings = listings.length;
+       const startIndex = numberOfListings;
+       const urlParams = new URLSearchParams(location.search);
+       urlParams.set("startIndex", startIndex);
+       const searchQuery = urlParams.toString();
+       const res = await fetch(`/api/listing/get?${searchQuery}`);
+       const data = await res.json();
+       if (data.length < 9) {
+         setShowMore(false);
+       }
+       setListings([...listings, ...data]);
+     };
 
 
   return (
@@ -201,9 +222,29 @@ export default function Search() {
           Listing results:
         </h1>
         <div className="p-7 flex flex-wrap justify-evenly gap-10 ">
-          { !loading && listings.length === 0 && (<p className="text-xl text-mixup text-center w-full font-semibold">No listing found!</p> )}
-          {loading && ( <p className="text-xl text-mixup text-center w-full font-semibold">Loading...</p>)}
-          {!loading && listings && listings.map((listing) => <ListingItem key={listing._id} listing={listing} />)}
+          {!loading && listings.length === 0 && (
+            <p className="text-xl text-mixup text-center w-full font-semibold">
+              No listing found!
+            </p>
+          )}
+          {loading && (
+            <p className="text-xl text-mixup text-center w-full font-semibold">
+              Loading...
+            </p>
+          )}
+          {!loading &&
+            listings &&
+            listings.map((listing) => (
+              <ListingItem key={listing._id} listing={listing} />
+            ))}
+          {showMore && (
+            <button
+              onClick={onShowMoreClick}
+              className="text-mixup hover:underline p-7 text-center w-full"
+            >
+              Show more....
+            </button>
+          )}
         </div>
       </div>
     </div>
