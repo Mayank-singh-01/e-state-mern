0a027972bcb2fb79876444d5fd78a8c6ae98bb88
Mayank Singh <mayank23378@gmail.com>
2024-09-07 00:18:42 +0530

    another bug fix

--- api/controllers/User.controller.js
+++ api/controllers/User.controller.js
@@ -1,5 +1,5 @@
 import bcryptjs from 'bcryptjs';
-import User from "../models/user.model.js";
+import User from "../models/User.model.js";
 import { errorHandler } from "../utils/Error.js";
 import Listing from '../models/listing.model.js';
 
