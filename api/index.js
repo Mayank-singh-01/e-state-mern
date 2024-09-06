08296438aef129f15bb54e1108b74f1d2b207d4c
Mayank Singh <mayank23378@gmail.com>
2024-09-07 00:30:35 +0530

    hope its a last error for deployment

--- api/index.js
+++ api/index.js
@@ -28,10 +28,10 @@ app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter)
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
+app.use(express.static(path.join(__dirname, "/clint/dist")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+  res.sendFile(path.join(__dirname, "clint", "dist", "index.html"));
 });
 
 //middleware 
