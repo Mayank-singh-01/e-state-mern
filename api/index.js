c40fd12a1599ce17f0c846215d62a92abced18f5
Mayank Singh <mayank23378@gmail.com>
2024-09-06 22:33:35 +0530

    pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/User.route.js';
 import authRouter from './routes/Auth.route.js';
 import listingRouter from './routes/listing.route.js'
 import cookieParser from "cookie-parser";
+import path from 'path';
 
 //config env 
 dotenv.config();
@@ -16,6 +17,8 @@ mongoose.connect(process.env.MONGO_DB).then(()=>{
    console.log(error);
 });
 
+const __dirname = path.resolve();
+
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
@@ -24,6 +27,12 @@ app.use(cookieParser());
 app.use('/api/user',userRouter);
 app.use('/api/auth',authRouter);
 app.use('/api/listing',listingRouter)
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 //middleware 
 app.use((err,req,res,next) => {
