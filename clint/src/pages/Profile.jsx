6ae588e99a9b958af357dbdae3749204484b78b8
Mayank Singh <mayank23378@gmail.com>
2024-09-24 10:19:04 +0530

    fix UI

--- clint/src/pages/Profile.jsx
+++ clint/src/pages/Profile.jsx
@@ -218,7 +218,7 @@ export default function Profile() {
           {loading ? "Loading..." : "Update"}
         </button>
         <Link
-          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95"
+          className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95 mb-4"
           to={"/create-listing"}
         >
           Create Listing
@@ -239,10 +239,10 @@ export default function Profile() {
         </span>
       </div> */}
 
-      <p className="text-red-700 mt-5">{error ? error : ""}</p>
+      {/* <p className="text-red-700 mt-5">{error ? error : ""}</p>
       <p className="text-green-700 mt-5">
         {updateSuccess ? "User is updated successfully!" : ""}
-      </p>
+      </p> */}
       <button
         onClick={handleShowListings}
         className="bg-green-700 text-white p-3 rounded-lg uppercase text-center hover:opacity-95 w-full"
@@ -307,6 +307,10 @@ export default function Profile() {
           Sign out
         </span>
       </div>
+      <p className="text-red-700 mt-5 text-center">{error ? error : ""}</p>
+      <p className="text-green-700 mt-5 text-center">
+        {updateSuccess ? "User is updated successfully!" : ""}
+      </p>
     </div>
   );
 }
